00344aeae145139b65fee532d75e7b7e94d2af56
Jerald Nellas <74941327+cj211993@users.noreply.github.com>
2021-01-09 02:40:48 +0800

    Add files via upload

---
+++ OPTIMUM CWB-L3/DOM/document.js
@@ -0,0 +1,16 @@
+let value;
+
+value = document;
+value = document.all;
+value = document.all[2];
+value = document.all.length;
+value = document.head;
+value = document.body;
+value = document.domain;
+value = document.URL;
+value = document.characterSet;
+value = document.contentType;
+
+
+
+console.log(value);
---
+++ OPTIMUM CWB-L3/JAVA SCRIPT LESSON/variable.js
@@ -0,0 +1,21 @@
+// var, Let, Const
+
+var name = 'John Doe';
+console.log(name);
+name =  'Steve Smith'
+console.log(name);
+
+// Init var
+var greeting;
+console.log(greeting);
+var greeting = 'Hello'
+console.log(greeting)
+
+// letters, numbers, _, $
+// can not start with number
+
+// Multi word vars
+var firstName = 'John'; //camel case
+var first_name = 'Sarah'; //underscore
+var FirstName = 'Tom'; //Pascal Case
+
